7161f2f83b6eaffac8af14d23d720129b92d79a5
Akash-H-M <91738503+Akash-H-M@users.noreply.github.com>
2022-01-25 13:43:26 +0530

    Update DemoApplicationController.java

--- src/main/java/com/example/demo/DemoApplicationController.java
+++ src/main/java/com/example/demo/DemoApplicationController.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoApplicationController {
   
 	
-	@RequestMapping("/home")  
+	@GetMapping("/home")  
 	public String hello()   
 	{  
 	return "My first java application";  
